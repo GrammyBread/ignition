4d32544213008875d9083d656c5d5c70e1c8b775
Alex <alexandrea.like@gmail.com>
2022-11-21 17:14:47 -0500

    Added proxy

--- next.config.js
+++ next.config.js
@@ -3,5 +3,13 @@ module.exports = {
     domains: ['imgix.cosmicjs.com',
       'cdn.cosmicjs.com'
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/docs/:path*',
+        destination: 'http://localhost:3000/:path*' // Proxy to Backend
+      }
+    ]
   }
 }
---
+++ pages/api/ping.tsx
@@ -0,0 +1,5 @@
+const Ping = (): JSX.Element => {
+    return <div></div>
+}
+
+export default Ping
